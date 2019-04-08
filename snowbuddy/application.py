314ebcbeec7e3aba4c1763210c3e397f1794cc00
T Sheasha <tarek.sheasha@booking.com>
2019-04-08 14:23:32 +0200

    req

--- snowbuddy/application.py
+++ snowbuddy/application.py
@@ -2,9 +2,12 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import enum
 from sqlalchemy import Integer, Enum
 from flask import request, json
+from passlib.apps import custom_app_context as pwd_context
+
 
 app = Flask(__name__, instance_relative_config=True)
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,6 +15,8 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "snowbuddy.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 try:
     os.makedirs(app.instance_path)
@@ -32,6 +37,13 @@ class User(db.Model):
     ski_level = db.Column(db.Enum(ExperienceLevel))
     snowboard_level = db.Column(db.Enum(ExperienceLevel))
     num_children = db.Column(db.Integer)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     def __repr__(self):
         return '<User {0} {1} {2} {3} {4} {5}>'.format(self.id, self.first_name,
@@ -45,6 +57,8 @@ def register():
         ski_level = request.json['ski_level'],
         snowboard_level = request.json['snowboard_level'],
         num_children = request.json['num_children'])
+    password = request.json['password']
+    user.hash_password(password)
     db.session.add(user)
     db.session.commit()
     response = app.response_class(
